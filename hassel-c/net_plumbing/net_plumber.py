9c8be61666528a1bb4394aee2bc3000195dae66d
brianliuya <leaderrorme@gmail.com>
2017-04-17 14:09:33 -0500

    Adding ryu using OpenFlow v1.3

--- hassel-c/net_plumbing/net_plumber.py
+++ hassel-c/net_plumbing/net_plumber.py
@@ -9,8 +9,31 @@ ProbeNode,SourceReachabilityProbeNode
 from net_plumber_process import *
 from multiprocessing import cpu_count, Event, JoinableQueue as jQueue
 from utils.wildcard import wildcard_intersect
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
 
-NUM_THREADS = 1#2*cpu_count() - 1
+
+yUM_THREADS = 1#2*cpu_count() - 1
+
+class L2Switch(app_manager.RyuApp):
+    def __init__(self, *args, **kwargs):
+         super(L2Switch, self).__init__(*args, **kwargs)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+        msg = ev.msg
+        dp = msg.datapath
+        ofp = dp.ofproto
+        ofp_parser = dp.ofproto_v1_3__parser # using OFv1.3
+
+        actions = [ofp_parser.OFPActionOutput(ofp.OFPP_FLOOD)]
+        out = ofp_parser.OFPPacketOut(
+            datapath=dp, buffer_id=msg.buffer_id, in_port=msg.in_port,
+            actions=actions)
+        dp.send_msg(out)
+
 
 class NetPlumber(object):
     '''
