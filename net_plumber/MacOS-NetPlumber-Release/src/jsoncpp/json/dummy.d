298defbdc3874158765ff726cba22b79b758c055
Br1an6 <leaderrorme@gmail.com>
2017-11-27 19:51:50 -0600

    Clean up

--- net_plumber/MacOS-NetPlumber-Release/src/jsoncpp/json/dummy.d
+++
@@ -1,4 +0,0 @@
-src/jsoncpp/json/dummy.d src/jsoncpp/json/dummy.o:  \
- ../src/jsoncpp/json/dummy.cc ../src/jsoncpp/json/json.h
-
-../src/jsoncpp/json/json.h:
--- net_plumber/setup-ubuntu.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt-get install liblog4cxx10
-sudo apt-get install liblog4cxx10-dev
-sudo apt-get install libcppunit-1.12-1
-sudo apt-get install libcppunit-dev
